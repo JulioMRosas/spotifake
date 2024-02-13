4a73aad57aa79bd0196750efd3d7769a92bf1663
Ibby <al21760482@ite.edu.mx>
2024-02-13 09:22:24 -0800

    configuracion basica del homepage

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./navbar.module.css"
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+        Navbar
+    </nav>
+  )
+}
+
+export default Navbar;
