621685189fd1262fc2bd91ce027f001d782ca35b
Daniela Hernandez <xdannix2626@gmail.com>
2024-02-14 22:25:03 -0800

    subiendo reproductor de musica con colores

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import styles from './footer.module.css'
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
 
 export const Footer = () => {
   return (
-    <div className ={styles.footer}>Footer</div>
-  )
+    <div className={styles.footer}>
+      <SkipPreviousIcon style={{ fontSize: "70px", marginTop: '-55px' }} />
+      <PlayCircleIcon style={{ fontSize: "70px", marginTop: '-55px'}} />
+      <SkipNextIcon style={{ fontSize: "70px", marginTop: '-55px' }} />
+    </div>
+  );
 }
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
     <nav className={styles.navbar}>
          <div className={styles.sidebar}>
         <ul>
-          <li><a href="#categoria1">Categoría 1</a></li>
-          <li><a href="#categoria2">Categoría 2</a></li>
-          <li><a href="#categoria3">Categoría 3</a></li>
+          <li>Categoría 1</li>
+          <li>Categoría 2</li>
+          <li>Categoría 3</li>
         </ul>
       </div>
     </nav>
