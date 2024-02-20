7ee7e3e8fa2f79e6c33199fc429f1a2ec56a27d0
SantanaLicon <spor@outlook.cl>
2024-02-20 10:08:23 -0800

    Se hizo los cambios al navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import styles from "./navbar.module.css";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.sidebar}>
-        <ul>
-          NavBar
-          <li className={styles.Perfil}>Perfil de usuario</li>
-          <li>Biblioteca</li>
-          <li>Configuracion</li>
-        </ul>
+        <div>
+          <div className={styles.Logo}><Image
+          src="/SpaceMelody.svg"
+          width={150}
+          height={150}
+          alt="logo image"
+          className={styles.Imagen}
+          /></div>
+          <div className={styles.Titulo}>🌌Space Melody🪐</div>
+          <div className={styles.Perfil}>⭐Perfil de usuario⭐</div>
+          <div className={styles.Biblioteca}>🌠Biblioteca🌠</div>
+          <div className={styles.Configuracion}>🌟Configuracion🌟</div>
+        </div>
       </div>
     </nav>
   );
