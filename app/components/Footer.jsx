4f2ce6775f31b5d73a599af1a0308187646fbcbc
Daniela Hernandez <xdannix2626@gmail.com>
2024-02-20 10:24:11 -0800

    agregando progreso de footer y diseño :fire:

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -10,6 +10,19 @@ export const Footer = () => {
       <SkipPreviousIcon style={{ fontSize: "70px", marginTop: "-55px" }} />
       <PlayCircleIcon style={{ fontSize: "70px", marginTop: "-55px" }} />
       <SkipNextIcon style={{ fontSize: "70px", marginTop: "-55px" }} />
+      <div className={styles.ContenedorPadredeFotoeInfo}>
+        <div className={styles.divContenedordeAlbumFoto}>
+          <div className={styles.divCuadradoBlanco}></div>
+          <div className={styles.divEspaciado}></div>
+        </div>
+        <div className={styles.divNombreCancionArtistaCont}>
+          <span className={styles.nombreCancion}>Nombre de la canción</span>
+          <span className={styles.nombreArtista}>Nombre del artista</span>
+        </div>
+      </div>
+      <div className={styles.progressBar}>
+        <div className={styles.progressBarInterno}></div>
+      </div>
     </div>
   );
 };
